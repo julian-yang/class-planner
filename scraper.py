e25048f9aadb45046f31edad2cf2470cc61045b9
stackedqueue <yang.julian@gmail.com>
2015-01-04 01:00:00 -0800

    added detection for no classes in a major, a list of these majors are printed at the end of insertMajors();\nfixed bug where & was not being translated to %26 causing majors with codes like 'ARCH&UD' (Architecture and Urban Design) to fail

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ import re
 import logUtility
 
 MAJOR_LIST_URL = 'http://www.registrar.ucla.edu/schedule/schedulehome.aspx'
-COURSE_LIST_URL = 'http://www.registrar.ucla.edu/schedule/crsredir.aspx?termsel=14S&subareasel=' # COM+SCI
+COURSE_LIST_URL = 'http://www.registrar.ucla.edu/schedule/crsredir.aspx?termsel='+CUR_TERM+'&subareasel=' # COM+SCI
 # DEFAULT TAG NAMES HERE
 MAJOR_LIST_WRAPPER_ID = 'ctl00_BodyContentPlaceHolder_SOCmain_lstSubjectArea'
 CLASS_LIST_WRAPPER_ID = 'ctl00_BodyContentPlaceHolder_crsredir1_lstCourseNormal'
@@ -13,7 +13,7 @@ CLASS_LIST_WRAPPER_ID = 'ctl00_BodyContentPlaceHolder_crsredir1_lstCourseNormal'
 MAJOR_XPATH = '//select[@id="' + MAJOR_LIST_WRAPPER_ID + '"]/*'
 COURSE_XPATH = '//select[@id="' + CLASS_LIST_WRAPPER_ID + '"]/*'
 # OTHER VARS HERE
-CUR_TERM = '14S'
+CUR_TERM = '15W'
 
 
 
@@ -30,8 +30,8 @@ def getPageTree(url):
 
 def clearTables():
     logUtility.title('Deleting current entries')
-    print 'Deleted', db.course.delete().execute(), 'major(s)'
-    print 'Deleted', db.major.delete().execute(), 'course(s)'
+    print 'Deleted', db.course.delete().execute(), 'course(s)'
+    print 'Deleted', db.major.delete().execute(), 'major(s)'
     print '...done\n'
 
 def insertMajors():
@@ -75,26 +75,42 @@ def insertCourses():
 
     majors = db.major.select()
     db.myDB.connect()
-
+    
+    numCourses = 0
+    courselessMajors = []
     for curMajor in majors:
         print '>>> Processing', curMajor.major_code, 'courses'
-        tree = getPageTree(COURSE_LIST_URL + 'COM+SCI')
+        curMajorCode = re.sub(' ', '+', curMajor.major_code)
+        curMajorCode = re.sub('&', '%26', curMajorCode)
+        tree = getPageTree(COURSE_LIST_URL + curMajorCode)
         courseCodes = tree.xpath(COURSE_XPATH + '/@value')
         courseNames = tree.xpath(COURSE_XPATH + '/text()')
         courseList = map((lambda (a,b): (a.strip(), b.strip())),\
                 zip(courseCodes, courseNames))
+        courseListLen = len(courseList)
         
+        if(courseListLen == 0):
+            print 'No courses this quarter, moving on to next major\n'
+            courselessMajors.append(curMajor.major_name)
+            continue
+
         row_dict = [{ \
             'major': curMajor, \
             'course_code': code, \
             'course_name': name, \
             } for (code,name) in courseList]
         insertQuery = db.course.insert_many(row_dict)
-            
+        print 'Inserting', courseListLen, 'courses' 
         print 'Insert new classes: first primary key inserted =', insertQuery.execute()
         print ''
+        numCourses += courseListLen
     # end for
     db.myDB.close()
+    print 'Added', numCourses, 'courses'
+    print 'The following majors have no courses this quarter:'
+    for major in courselessMajors:
+        print '-', major
+
     print '...done\n'
 
 
